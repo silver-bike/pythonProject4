10fd3abc7d24976e1235cf631f87f81c3733b1c5
silverbike <silverbike.python@gmail.com>
2024-05-25 01:08:29 +0400

    using panda for table

--- bina.py
+++ bina.py
@@ -1,4 +1,15 @@
 import pandas as pd
 import numpy as np
 
- df = pd.dataframe()
+data = np.zeros((120, 7))
+
+with open('text.txt') as file:
+    indx = 0
+    for row in file:
+        lst_text = list(map(float, row.split()))
+        data[indx] = lst_text
+        indx += 1
+
+df = pd.DataFrame(data=data, columns=['Month', 'Year', 'Due', 'Principal', 'Interest', 'Insurance', 'Balance'])\
+
+print(df)
