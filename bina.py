76ab9718b17d93ce0fa9339084cc25f2181be67e
silverbike <silverbike.python@gmail.com>
2024-05-25 01:53:10 +0400

    calculating year % monthly

--- bina.py
+++ bina.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-data = np.zeros((120, 7))
+data = np.zeros((121, 7))
 
 with open('text.txt') as file:
     indx = 0
@@ -10,6 +10,14 @@ with open('text.txt') as file:
         data[indx] = lst_text
         indx += 1
 
-df = pd.DataFrame(data=data, columns=['Month', 'Year', 'Due', 'Principal', 'Interest', 'Insurance', 'Balance'])\
+df = pd.DataFrame(data=data, columns=['Month', 'Year', 'Due', 'Principal', 'Interest', 'Insurance', 'Balance'])
 
-print(df)
+df['Previous_Balance'] = df.Principal + df.Balance
+
+df['%'] = 100 * (((df.Balance + df.Due - df.Insurance) / df.Previous_Balance) ** 12 - 1)
+
+df_new = df[['Due', 'Principal', 'Interest', 'Insurance', 'Balance', 'Previous_Balance', '%']]
+
+
+pd.set_option('display.max_rows', None)
+print(df_new)
